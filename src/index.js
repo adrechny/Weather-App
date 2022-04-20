eeff3cc3f6631810389ae04437f779b84ca1d3d8
adrechny <adrechny311@gmail.com>
2022-04-19 20:32:00 -0500

    Added HTML, CSS, & JS files from VS Code

---
+++ src/index.js
@@ -0,0 +1,61 @@
+let currentTime = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let currentHours = currentTime.getHours();
+if (currentHours < 10) {
+  currentHours = `0${currentHours}`;
+}
+let currentMinutes = currentTime.getMinutes();
+if (currentMinutes < 10) {
+  currentMinutes = `0${currentMinutes}`;
+}
+let currentDay = days[currentTime.getDay()];
+let today = document.querySelector(".day");
+today.innerHTML = `${currentDay}`;
+let timeNow = document.querySelector(".time");
+timeNow.innerHTML = `${currentHours}:${currentMinutes}`;
+
+function showWeatherDetails(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector(".temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector(".humidity").innerHTML = response.data.main.humidity;
+  document.querySelector(".wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector(".condition").innerHTML =
+    response.data.weather[0].main;
+}
+function searchCity(city) {
+  let apiKey = "3f49c533f868ff71e6ec9af59e37e163";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeatherDetails);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
+}
+function searchLocation(position) {
+  let apiKey = "3f49c533f868ff71e6ec9af59e37e163";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeatherDetails);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let search = document.querySelector(".search-bar");
+search.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector(".current-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+searchCity("Chicago");

2ec18027adde3f4f2ad967378bf4887bfa97b5a6
adrechny <adrechny311@gmail.com>
2022-04-28 20:20:34 -0500

    Added forecast in JS and removed from HTML

--- src/index.js
+++ src/index.js
@@ -22,6 +22,32 @@ today.innerHTML = `${currentDay}`;
 let timeNow = document.querySelector(".time");
 timeNow.innerHTML = `${currentHours}:${currentMinutes}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `  
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-min">27°</span>
+                |
+                <span class="weather-forecast-temperature-max">37°</span>
+                <img src="https://openweathermap.org/img/wn/50d@2x.png" alt="" width="55px">
+                </div>
+              </div>
+            `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function showWeatherDetails(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   celsiusTemperature = response.data.main.temp;
@@ -94,3 +120,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("Chicago");
+displayForecast();
